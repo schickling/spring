f5f80a52ba7e6f7b05d6e7c8324d238bf9b0d5a8
Johannes Schickling <schickling.j@gmail.com>
2014-02-11 00:20:37 +0100

    Animate

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var canvas = document.getElementsByTagName('canvas')[0],
     height = window.innerHeight,
     r = Math.random,
     xGap = 50,
-    colors = ['#D1F2A5', '#EFFAB4', '#FFC48C', '#FF9F80', '#F56991'];
+    colors = ['#D1F2A5', '#EFFAB4', '#FFC48C', '#FF9F80', '#F56991'],
+    t = 0,
     lines = [];
     
 init();
@@ -17,35 +18,30 @@ function init() {
     lines[0] = createLine();
     lines[1] = createLine();
 
-    //setInterval(function() {
-        ////refreshLine(lines[0]);
-        ////refreshLine(lines[1]);
-        //draw();
-    //}, 20);
+    setInterval(function() {
+        t += r() * 0.001 + 0.002;
+        draw();
+    }, 20);
 }
 
 function createLine() {
     var line = [],
-        x = 0
+        x = -20 * xGap,
         y = 0;
 
     while (x < width + 2 * xGap) {
         y = rand(y);
         var coord = {
             x: x,
-            y: y
+            y: y,
+            color: colors[Math.floor(r() * colors.length)],
+            sign: sign(0.5 - r())
         };
         line.push(coord);
         x += xGap * (1 + r());
     }
 
     return line;
-}
-
-function refreshLine(line) {
-    for (var i = 0; i < line.length; i++) {
-        line[i].y = shift(line[i].y, 10);
-    }
 }
 
 function draw() {
@@ -64,11 +60,11 @@ function fillPolygons(line1, line2) {
 }
 
 function fillPolygon(vertices) {
-    ctx.fillStyle = colors[Math.floor(r() * colors.length)];
+    ctx.fillStyle = vertices[0].color;
     ctx.beginPath();
-    ctx.moveTo(vertices[0].x, vertices[0].y);
+    //ctx.moveTo(vertices[0].x, vertices[0].y);
     for (var i = vertices.length - 1; i >= 0; i--) {
-        ctx.lineTo(vertices[i].x, vertices[i].y);
+        ctx.lineTo(shift(vertices[i].x, vertices[i].sign, 40), shift(vertices[i].y, vertices[i].sign, 100));
     } 
     ctx.closePath();
     ctx.fill();
@@ -78,8 +74,8 @@ function rand (prev) {
     return height / 2 * (0.5 + r());
 }
 
-function shift(value, offset) {
-    return value + offset * (0.5 - r());
+function shift(value, sgn, offset) {
+    return value + sgn * Math.sin(t * 0.7) * offset;
 }
 
 
@@ -89,4 +85,8 @@ function drawLine(line) {
         ctx.lineTo(line[i].x, line[i].y);
     }
     ctx.stroke();
+}
+
+function sign(number) {
+    return number < 0 ? -1 : 1;
 }
